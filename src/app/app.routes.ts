be1c66cf6d368f96b316a1a84fd154ab27c24b4c
Veronica Camila Zapata <vero.camiz@hotmail.com>
2026-03-06 18:04:09 -0300

    changed styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Fase2Component } from './pages/fase2/fase2';
 import { Fase3Component } from './pages/fase3/fase3';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: Fase2Component },
   { path: 'fase2', component: Fase2Component },
   { path: 'fase3', component: Fase3Component }
 ];
