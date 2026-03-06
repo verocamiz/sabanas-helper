33fce848fbc210af7f180506ef131516cc2d1faf
Veronica Camila Zapata <vero.camiz@hotmail.com>
2026-03-06 20:43:12 -0300

    moved footer to another component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { FormsModule } from '@angular/forms';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar,FormsModule],
+  imports: [RouterOutlet, Navbar,FormsModule,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
