089396cfb2dc9a4a2a46521f9429c62d144c0605
Veronica Camila Zapata <vero.camiz@hotmail.com>
2026-03-06 17:44:01 -0300

    added fase3 styles

--- src/app/components/fase2-calculator/fase2-calculator.ts
+++ src/app/components/fase2-calculator/fase2-calculator.ts
@@ -1,13 +1,13 @@
 // src/app/pages/fase2/fase2-calculator.component.ts
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 import { TotemService, Estado } from '../../services/totem';
 
 @Component({
   selector: 'app-fase2-calculator',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,TitleCasePipe],
   templateUrl: './fase2-calculator.html'
 })
 export class Fase2CalculatorComponent {
@@ -28,4 +28,16 @@ export class Fase2CalculatorComponent {
     this.resultados = r.resultados;
     this.estadoBoss = r.estadoBoss;
   }
+
+  // Define el mapeo de estilos e iconos (Ajustado a Font Awesome v5)
+getEstilo(elemento: string) {
+  const mapa: any = {
+    'fuego':  { icon: 'fas fa-fire',     css: 'text-orange-600', bg: 'bg-orange-50',  border: 'border-orange-500' },
+    'agua':   { icon: 'fas fa-tint',     css: 'text-blue-600',   bg: 'bg-blue-50',    border: 'border-blue-500' },
+    'tierra': { icon: 'fas fa-leaf',     css: 'text-green-700',  bg: 'bg-green-50',   border: 'border-green-600' },
+    'aire':   { icon: 'fas fa-wind',     css: 'text-sky-400',    bg: 'bg-sky-50',     border: 'border-sky-300' }
+  };
+  // Valor por defecto por si el elemento no coincide
+  return mapa[elemento] || { icon: 'fas fa-question-circle', css: 'text-gray-500', bg: 'bg-gray-100', border: 'border-gray-300' };
+}
 }
--- src/app/components/fase3-calculator/fase3-calculator.ts
+++ src/app/components/fase3-calculator/fase3-calculator.ts
@@ -27,4 +27,20 @@ export class Fase3CalculatorComponent {
     this.resultados = r.resultados;
     this.estadoBoss = r.estadoBoss;
   }
+
+  getEstilo(elemento: string) {
+  const mapa: any = {
+    'fuego':  { icon: 'fas fa-fire',     css: 'text-orange-600', bg: 'bg-orange-50',  border: 'border-orange-500' },
+    'agua':   { icon: 'fas fa-tint',     css: 'text-blue-600',   bg: 'bg-blue-50',    border: 'border-blue-500' },
+    'tierra': { icon: 'fas fa-leaf',     css: 'text-green-700',  bg: 'bg-green-50',   border: 'border-green-600' },
+    'aire':   { icon: 'fas fa-wind',     css: 'text-sky-400',    bg: 'bg-sky-50',     border: 'border-sky-300' },
+    'fuegomaldito':  { icon: 'fas fa-fire',     css: 'text-orange-600', bg: 'bg-orange-50',  border: 'border-orange-500' },
+    'aguamaldita':   { icon: 'fas fa-tint',     css: 'text-blue-600',   bg: 'bg-blue-50',    border: 'border-blue-500' },
+    'tierramaldita': { icon: 'fas fa-leaf',     css: 'text-green-700',  bg: 'bg-green-50',   border: 'border-green-600' },
+    'airemaldito':   { icon: 'fas fa-wind',     css: 'text-sky-400',    bg: 'bg-sky-50',     border: 'border-sky-300' }
+  };
+  // Valor por defecto por si el elemento no coincide
+  return mapa[elemento] || { icon: 'fas fa-question-circle', css: 'text-gray-500', bg: 'bg-gray-100', border: 'border-gray-300' };
+}
+
 }
